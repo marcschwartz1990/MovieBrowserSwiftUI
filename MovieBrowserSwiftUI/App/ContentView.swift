9e63ab20819bddb291a81fde77a0513cec1ec3a6
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-23 21:42:56 -0400

    Added URLComponents in func loadData, results variable still not producing any results.

--- MovieBrowserSwiftUI/App/ContentView.swift
+++ MovieBrowserSwiftUI/App/ContentView.swift
@@ -62,53 +62,62 @@ import SwiftUI
 
 // Movie DB Request
 
-//struct Response: Codable {
-//    var results: [Result]
-//}
-//
-//struct Result: Codable {
-//    var original_title: String
-//    var release_date: String
-//}
-//
-//struct ContentView: View {
-//    @State private var results = [Result]()
-//
-//    var body: some View {
-//        Text("Start")
-//        List(results, id: \.original_title) { item in
-//            VStack(alignment: .leading) {
-//                Text(item.original_title)
-//                    .font(.headline)
-//                Text(item.release_date)
-//            }
-//        }
-//        .task {
-//            await loadData()
-//        }
-//    }
-//
-//    func loadData() async {
-//        guard let url = URL(string:
-//            "https://api.themoviedb.org/3/movie/550?api_key=f1bba69d2f7b8e57f3db0f3c5cf16c9a") else
-//            {
-//            print("Invalid URL")
-//            return
-//        }
-//
-//        do {
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//
-//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-//                results = decodedResponse.results
-//
-//            }
-//        } catch {
-//            print("Invalid data")
-//        }
-//        print(results)
-//    }
-//}
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var original_title: String
+    var release_date: String
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        Text("Top")
+        List(results, id: \.original_title) { item in
+            VStack(alignment: .leading) {
+                Text(item.original_title)
+                    .font(.headline)
+                Text(item.release_date)
+            }
+        }
+        .task {
+            await loadData()
+        }
+        Text("Bottom")
+    }
+
+    func loadData() async {
+        var components = URLComponents()
+
+        components.scheme = "https"
+        components.host = "api.themoviedb.org"
+        components.path = "/search/movie"
+        components.queryItems = [
+            URLQueryItem(name: "api_key", value: "f1bba69d2f7b8e57f3db0f3c5cf16c9a"),
+            URLQueryItem(name: "query", value: "Jack+Reacher")
+        ]
+
+        guard let url = components.url else {
+            print("Invalid URL")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+
+            }
+        } catch {
+            print("Invalid data")
+        }
+        print("results: \(results)") // Printing for debugging purposes
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
